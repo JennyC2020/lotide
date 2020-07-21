787a9e7483b529ce4c782ba599f59e9473b20993
Jenny Citherlet <jennycith@gmail.com>
2020-07-21 15:25:53 +0000

    message

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,14 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log("✳️✳️✳️ Assertion Passed: " + actual + "===" + expected);
+    console.log(`✳️ ✳️ ✳️ Assertion Passed:  ${actual} ===  ${expected}`);
   } else {
-    console.log("🔴🔴🔴 Assertion Failed: " + actual + "!==" + expected);
+    console.log(`🔴 🔴 🔴 Assertion Failed:  ${actual} ===  ${expected}`);
   }
 
   return assertEqual;
 };
 
 // TEST CODE
-console.assert(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.assert(assertEqual('Lighthouse Labs', 'Bootcamp'));
 console.assert(assertEqual(1, 1));
---
+++ tail.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const tail = function (actual, expected) {
+  if (actual[0] === expected) {
+    console.log(actual.slice(1));
+  } else {
+    console.log(actual.slice(1));
+  }
+
+  return tail;
+};
+
+// TEST CODE
+console.assert(tail([5, 6, 7], 5));
+console.assert(tail(['Hello', 'Lighthouse', 'Labs'], 'Hello'));
