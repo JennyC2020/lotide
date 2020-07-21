0094ecbc611facf61c9a0794743ae1dc678ed384
Jenny Citherlet <jennycith@gmail.com>
2020-07-21 04:03:38 +0000

    Initial function that output the first element of array

---
+++ head.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual[0] === expected) {
+    console.log(actual[0]);
+  } else {
+    console.log(actual[0]);
+  }
+
+  return assertEqual;
+};
+
+// TEST CODE
+console.assert(assertEqual([5, 6, 7], 5));
+console.assert(assertEqual(['Hello', 'Lighthouse', 'Labs'], 'Hello'));
