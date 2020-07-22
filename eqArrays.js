331a25c1de118fe12c2017c96f6b678c9d026e18
Jenny Citherlet <jennycith@gmail.com>
2020-07-22 00:11:14 +0000

    Initial commit of function checking if 2 arrays are a perfect match

---
+++ eqArrays.js
@@ -0,0 +1,35 @@
+/* To do
+assign a variable for array1
+assign a variable for array2
+
+compare the 2 of them
+output should be bolean
+
+*/
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✳️ ✳️ ✳️ Assertion Passed:  ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🔴 🔴 🔴 Assertion Failed:  ${actual} ===  ${expected}`);
+  }
+
+  return assertEqual;
+};
+
+const eqArrays = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
