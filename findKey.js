247d72833f611ab9b172c87f499a5ad2a058fa9d
Jenny Citherlet <jennycith@gmail.com>
2020-07-24 04:02:28 +0000

    Inital commit

---
+++ findKey.js
@@ -0,0 +1,34 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✳️ ✳️ ✳️ Assertion Passed:  ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🔴 🔴 🔴 Assertion Failed:  ${actual} ===  ${expected}`);
+  }
+
+  return assertEqual;
+};
+
+var findKeyIn = {
+  'Blue Hill': { stars: 1 },
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 },
+};
+
+var findKey = function (object, callback) {
+  for (var item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+
+  return undefined;
+};
+
+var found = findKey(findKeyIn, (item) => item.stars === 2);
+console.log(found);
+
+assertEqual(found, 'noma');
