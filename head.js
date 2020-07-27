9264a4ec9139c50476df9927bb1f6807f932629c
Jenny Citherlet <jennycith@gmail.com>
2020-07-27 20:30:13 +0000

    Shortened code and added module export

--- head.js
+++ head.js
@@ -1,20 +1,5 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual[0] === expected) {
-    console.log(actual[0]);
-  } else {
-    console.log(actual[0]);
-  }
-
-  return assertEqual;
-};
-
-
-// Function head
 const head = function(array) {
   return array[0];
 };
 
-// TEST CODE
-console.assert(assertEqual([5, 6, 7], 5));
-console.assert(assertEqual(['Hello', 'Lighthouse', 'Labs'], 'Hello'));
+module.exports = head;
