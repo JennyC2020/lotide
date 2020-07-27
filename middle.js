ce9c20c7bff9cdf766a5f90d69ded8dbd6cbea33
Jenny Citherlet <jennycith@gmail.com>
2020-07-27 21:58:13 +0000

    Refacted code and added middle export module

--- middle.js
+++ middle.js
@@ -1,36 +1,5 @@
-// // TEST/ASSERTION FUNCTIONS
-
-const eqArrays = function (array1, array2) {
-  if (array1.length === 0 && array2.length === 0) {
-    return true;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-};
-
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✳️ ✳️ ✳️ Assertion Passed:  ${actual} ===  ${expected}`);
-  } else {
-    console.log('actual', actual);
-    console.log('expected', expected);
-    console.log(`🔴 🔴 🔴 Assertion Failed:  ${actual} ===  ${expected}`);
-  }
-};
-
-//Function to find the middleOfArray elements
-// function takes in an of number
-// returns an array of the middle elements
-const middle = function (array) {
-  var middleOfArray = Math.floor(array.length / 2); //Split the array in half
-
-  //console.log(array); //log the contents of the array
-  //console.log('array length is: ' + array.length);
+const middle = function(array) {
+  var middleOfArray = Math.floor(array.length / 2);
 
   if (array.length === 1 || array.length === 2) {
     console.log('[]');
@@ -43,12 +12,4 @@ const middle = function (array) {
     return [array[middleOfArray]];
   }
 };
-console.log('!!!!!', middle([1]));
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
-// middle([1]); // => []
-// middle([1, 2]); // => []
-// middle([1, 2, 3]); // => [2]
-// middle([1, 2, 3, 4, 5]); // => [3]
-// middle([1, 2, 3, 4]); // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+module.exports = middle;
