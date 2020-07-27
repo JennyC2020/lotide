b4bfc44607cc7d7aaf1948bc890c1ff75cec8d6a
Jenny Citherlet <jennycith@gmail.com>
2020-07-27 21:24:49 +0000

    Added import of assertEqual at the top

--- eqArrays.js
+++ eqArrays.js
@@ -1,22 +1,4 @@
-/* To do
-assign a variable for array1
-assign a variable for array2
-
-compare the 2 of them
-output should be bolean
-
-*/
-
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✳️ ✳️ ✳️ Assertion Passed:  ${actual} ===  ${expected}`);
-  } else {
-    console.log(`🔴 🔴 🔴 Assertion Failed:  ${actual} ===  ${expected}`);
-  }
-
-  return assertEqual;
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
@@ -30,8 +12,4 @@ const eqArrays = function(array1, array2) {
 
 module.exports = eqArrays;
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+
