899cc6e6e85aa36d19c7bbe488addae2979aa9bb
Jenny Citherlet <jennycith@gmail.com>
2020-07-27 20:39:13 +0000

    Inital commit of test for Tail function

---
+++ test/tailTest.js
@@ -0,0 +1,8 @@
+const tail = require("../tail");
+const assertEqual = require('../assertEqual');
+
+
+
+// TEST CODE
+assertEqual([5, 6, 7], 5);
+assertEqual(['Hello', 'Lighthouse', 'Labs'], 'hello');
