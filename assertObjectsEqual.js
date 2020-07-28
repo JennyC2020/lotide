4d75fc112cc32b88bcb4eb9fc3cc998056e5e9eb
Jenny Citherlet <jennycith@gmail.com>
2020-07-28 00:33:16 +0000

    Refracted and added export module

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,30 +1,3 @@
-// eqObjects
-
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  } else {
-    for (let key in object1) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (eqArrays(object1[key], object2[key]) === false) {
-          return false;
-        }
-      } else {
-        if (object1[key] !== object2[key]) {
-          return false;
-        }
-      }
-    }
-
-    return true;
-  }
-};
-
-// FUNCTION assertObjectsEqual
-
-/*
-
-*/
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
@@ -38,13 +11,3 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 module.exports = assertObjectsEqual;
-
-const ab = { a: '1', b: '2' };
-const ba = { b: '2', a: '1' };
-console.log(assertObjectsEqual(ab, ba)); // => true
-
-const abc = { a: '1', b: '2', c: '3' };
-console.log(assertObjectsEqual(ab, abc)); // => false
-
-assertObjectsEqual((ab, ba), true);
-assertObjectsEqual((ab, abc), false);
